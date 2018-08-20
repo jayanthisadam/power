b62c35ec32e959cadc2dcf0fda61622510d0deb1
jayanthisadam <41771096+jayanthisadam@users.noreply.github.com>
2018-08-20 21:08:36 +0530

    Update powe.py

--- powe.py
+++ powe.py
@@ -3,6 +3,6 @@ def power(a,b):
              return(a)
     if (b!=1):
              return(a*power(a,b-1))
-a = raw_input("")
-b = raw_input("")
+a=raw_input("")
+b=raw_input("")
 print(power(a,b))
